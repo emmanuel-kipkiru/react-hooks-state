738a04463776038354c334d2c64c17d91d22cf95
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-07-19 11:43:20 +0300

    commit tree 0b472cd4a65a7a3f15cd1068566279e9867a144f

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,17 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(`before setState: ${count}`);
+
     setCount(count + 1);
+    setCount(count + 1);
+
+    console.log(`after setState: ${count}`);
+
+    // console.log(count +" " + setCount);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>{count}</button>;
 }
 
 export default Counter;
